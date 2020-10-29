9f06e7574e22fc673eac168fcffd1d4c64f9ee26
Anurag Patil <aspanurag@gmail.com>
2020-10-29 13:59:11 -0400

    Lyrical GraphQL Initial Commit

---
+++ Lyrical-GraphQL/client/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import ApolloClient from "apollo-client";
+
+const Root = () => {
+  return <div>Lyrical</div>;
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
