dcff26bcbdb3fba3878ee19990875fbf6efad9e4
Anurag Patil <aspanurag@gmail.com>
2020-11-02 14:59:01 -0500

    add SongList Component

---
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import gql from "graphql-tag";
+class SongList extends Component {
+  render() {
+    return <div>SongList</div>;
+  }
+}
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default SongList;
--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import SongList from "./components/SongList";
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>Lyrical</div>;
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h1>Lyrical</h1>
+        <SongList />
+      </div>
+    </ApolloProvider>
+  );
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
