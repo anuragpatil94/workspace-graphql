3adb89702750900ba66d2e8e72874fc3af9535b1
Anurag Patil <aspanurag@gmail.com>
2020-11-02 15:27:56 -0500

    get list of songs from graphql

--- Lyrical-GraphQL/client/components/SongList.js
+++ Lyrical-GraphQL/client/components/SongList.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
 import gql from "graphql-tag";
+import { graphql } from "react-apollo";
 class SongList extends Component {
+  renderSongs() {
+    if (!this.props.data.loading)
+      return this.props.data.songs.map(({ id, title }, index) => (
+        <li key={id}>{title}</li>
+      ));
+    else return <p>Loading!!!</p>;
+  }
   render() {
-    return <div>SongList</div>;
+    return (
+      <div>
+        <h2>Songs List</h2>
+        {this.renderSongs()}
+      </div>
+    );
   }
 }
 
 const query = gql`
   {
     songs {
+      id
       title
     }
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
