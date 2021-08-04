ec1710f996467d1d9132fa533dfce1f92ef8a6c9
Anurag Patil <aspanurag@gmail.com>
2021-08-04 09:56:47 -0400

    create apollo server

--- GraphQL-With-Example/hello-world/server/server.js
+++ GraphQL-With-Example/hello-world/server/server.js
@@ -1,4 +1,4 @@
-const { gql } = require("apollo-server");
+const { gql, ApolloServer } = require("apollo-server");
 
 // GraphQL Schema Example
 const typeDefs = gql`
@@ -6,3 +6,23 @@ const typeDefs = gql`
     greeting: String
   }
 `;
+
+/** to check how server returns greeting value
+ * The `Query` property in resolvers resembles the `Query` property in typeDefs
+ * The `greeting` property in resolvers resembles the `greeting` property in typeDefs
+ * the typeDefs only will define the type of the property where as resolver will return actual value
+ */
+const resolvers = {
+  Query: {
+    greeting: () => "Hello World!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen({ port: 9000 })
+  .then(({ url }) => console.log(`Server running at URL:${url}`));
