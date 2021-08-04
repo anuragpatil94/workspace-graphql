27d50187394b29cd3a36df2455af3229527a5d13
Anurag Patil <aspanurag@gmail.com>
2021-08-04 10:43:28 -0400

    creating graphql client

---
+++ GraphQL-With-Example/hello-world/client/app.js
@@ -0,0 +1,22 @@
+const GRAPHQL_URL = "http://localhost:9000/";
+async function fetchGreeting() {
+  const response = await fetch(GRAPHQL_URL, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+        query{
+          greeting
+        }
+      `,
+    }),
+  });
+  const { data } = await response.json();
+  return data;
+}
+
+fetchGreeting().then((data) => {
+  document.querySelector("h1").textContent = data.greeting;
+});
