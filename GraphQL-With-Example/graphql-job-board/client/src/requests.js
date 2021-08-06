e30798988029445ae57e291ccc753e6e5796e94f
Anurag Patil <aspanurag@gmail.com>
2021-08-06 18:46:24 -0400

    refactor graphql request| error handling

--- GraphQL-With-Example/graphql-job-board/client/src/requests.js
+++ GraphQL-With-Example/graphql-job-board/client/src/requests.js
@@ -1,50 +1,49 @@
 const endPointURL = " http://localhost:9000/graphql";
 export async function loadJobs() {
-  const res = await fetch(endPointURL, {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify({
-      query: `
-        {
-          jobs{
-            id
-            title
-            company{
-              name
-            }
-          }
+  const query = `
+    {
+      jobs{
+        id
+        title
+        company{
+          name
         }
-      `,
-    }),
-  });
-
-  const responseBody = await res.json();
-  return responseBody.data.jobs;
+      }
+    }
+  `;
+  const { jobs } = await graphqlRequest(query);
+  return jobs;
 }
 
 export async function loadJob(id) {
+  const query = `
+    query JobQuery($id:ID!){
+      job(id: $id){
+        id
+        title
+        description
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { job } = await graphqlRequest(query, { id });
+  return job;
+}
+
+async function graphqlRequest(query, variables = {}) {
   const res = await fetch(endPointURL, {
     method: "POST",
     headers: { "content-type": "application/json" },
-
-    body: JSON.stringify({
-      query: `
-        query JobQuery($id:ID!){
-          job(id: $id){
-            id
-            title
-            description
-            company {
-              id
-              name
-            }
-          }
-        }
-      `,
-      variables: { id },
-    }),
+    body: JSON.stringify({ query, variables }),
   });
 
   const responseBody = await res.json();
-  return responseBody.data.job;
+  if (responseBody.errors) {
+    const message = responseBody.errors.map((err) => err.message).join("\n");
+    throw new Error(message);
+  }
+  return responseBody.data;
 }
