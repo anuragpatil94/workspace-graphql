69ee9af6345a7128aa078bab1b6a2d960b849fb1
Anurag Patil <aspanurag@gmail.com>
2021-08-06 11:37:12 -0400

    create nested queries

--- GraphQL-With-Example/graphql-job-board/server/resolvers.js
+++ GraphQL-With-Example/graphql-job-board/server/resolvers.js
@@ -2,6 +2,23 @@ const db = require("./db");
 
 const Query = {
   jobs: () => db.jobs.list(),
+  companies: () => db.companies.list(),
 };
 
-module.exports = { Query };
+/**
+ * Resolvers must reflect the schema.
+ * in schema `company`  is a field of `Job` type, we need to declare
+ * new resolver object of `job` type, where we can put functions to resolve the
+ * fields of `Job` type.
+ * The 1st param that this recieves is the parentObject (job)
+ */
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+
+const Company = {
+  jobs: (company) =>
+    db.jobs.list().filter((job) => job.companyId === company.id),
+};
+
+module.exports = { Query, Job, Company };
