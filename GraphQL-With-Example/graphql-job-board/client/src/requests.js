6ad9c97032a07e332c17ff4ec8a750147c7771ae
Anurag Patil <aspanurag@gmail.com>
2021-08-06 18:30:25 -0400

    get data for job details page with graphql query

--- GraphQL-With-Example/graphql-job-board/client/src/requests.js
+++ GraphQL-With-Example/graphql-job-board/client/src/requests.js
@@ -21,3 +21,30 @@ export async function loadJobs() {
   const responseBody = await res.json();
   return responseBody.data.jobs;
 }
+
+export async function loadJob(id) {
+  const res = await fetch(endPointURL, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+
+    body: JSON.stringify({
+      query: `
+        query JobQuery($id:ID!){
+          job(id: $id){
+            id
+            title
+            description
+            company {
+              id
+              name
+            }
+          }
+        }
+      `,
+      variables: { id },
+    }),
+  });
+
+  const responseBody = await res.json();
+  return responseBody.data.job;
+}
