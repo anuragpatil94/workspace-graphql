eba868ecfb9fe650dc26c13e36971175b5052d39
Anurag Patil <aspanurag@gmail.com>
2021-08-07 18:34:15 -0400

    update create job mutation use input variable as argument

--- GraphQL-With-Example/graphql-job-board/server/resolvers.js
+++ GraphQL-With-Example/graphql-job-board/server/resolvers.js
@@ -8,8 +8,8 @@ const Query = {
 };
 
 const Mutation = {
-  createJob: (root, { companyId, title, description }) => {
-    const id = db.jobs.create({ companyId, title, description });
+  createJob: (root, { input }) => {
+    const id = db.jobs.create(input);
     return db.jobs.get(id);
   },
 };
